b490f5205c79dcd9f1b3b168bb56fef26b6956e7
shad kinx <rivapermana763@gmail,com>
2024-03-13 06:16:24 +0700

    menambahkan implementasi dengan state-based construction

--- TPMOD4/Program.cs
+++ TPMOD4/Program.cs
@@ -1,23 +1,81 @@
-﻿public class KodePos
+﻿namespace TPMODUL04
 {
-    public enum EnumKelurahan
+    internal class Program
     {
-        Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja
-    }
-
-    public static int GetKodePos(EnumKelurahan kelurahan)
-    {
-        int[] KodePosKelurahan = { 40266, 40287, 40267, 40287, 40286, 40286, 40272, 40274, 40273 };
-        return KodePosKelurahan[(int) kelurahan];
-    }
-
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Kelurahan & Kode Pos");
-        Console.WriteLine("---------------------");
-        foreach (EnumKelurahan kelurahan in Enum.GetValues(typeof(EnumKelurahan)))
+        public class DoorMachine()
         {
-            Console.WriteLine($"{kelurahan} - {GetKodePos(kelurahan)}");
+            public enum StateDoor
+            {
+                TERBUKA, TERKUNCI
+            }
+
+        }
+
+        public class KodePos
+        {
+            private string[] kelurahans = { "Batununggal", "Kujangsari", "Cijarua", "Mengger", "Wates", "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer", "Samoja" };
+            private double[] kodepos = { 40266, 40287, 40287, 40267, 40256, 40286, 40286, 40286, 40272, 40274, 40273 };
+            public double getKodePos(string kelurahan)
+            {
+                for (int i = 0; i < kelurahans.Length; i++)
+                {
+                    if (kelurahans[i] == kelurahan)
+                    {
+                        return kodepos[i];
+                    }
+                }
+                return -1;
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            DoorMachine.StateDoor machine = DoorMachine.StateDoor.TERKUNCI;
+            Console.Write("Masukkan Perintah: BukaPintu/KunciPintu/Exit: ");
+            string input = Console.ReadLine();
+            while (input != "Exit")
+            {
+                switch (machine)
+                {
+                    case DoorMachine.StateDoor.TERBUKA:
+                        if (input == "KunciPintu")
+                        {
+                            machine = DoorMachine.StateDoor.TERKUNCI;
+                            Console.WriteLine("Pintu Terkunci");
+
+                        }
+                        break;
+                    case DoorMachine.StateDoor.TERKUNCI:
+                        if (input == "BukaPintu")
+                        {
+                            machine = DoorMachine.StateDoor.TERBUKA;
+                            Console.WriteLine("Pintu Tidak Terkunci");
+
+                        }
+                        break;
+                }
+                Console.Write("Input Perintah:  ");
+                input = Console.ReadLine();
+            }
+
+
+            KodePos kodePosObj = new KodePos();
+
+            string[] kelurahans = { "Batununggal", "Kujangsari", "Cijarua", "Mengger", "Wates", "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer", "Samoja" };
+
+            foreach (var kelurahan in kelurahans)
+            {
+                double kodepos = kodePosObj.getKodePos(kelurahan);
+                if (kodepos != -1)
+                {
+                    Console.WriteLine($"Kode pos {kelurahan}: {kodepos}");
+                }
+                else
+                {
+                    Console.WriteLine($"Kode pos untuk {kelurahan} tidak ditemukan");
+                }
+            }
         }
     }
 }
--- TPMOD4/obj/Debug/net8.0/TPMOD4.AssemblyInfo.cs
+++ TPMOD4/obj/Debug/net8.0/TPMOD4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TPMOD4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+355dc406bab269db83a5d26534d54431cd38ce55")]
 [assembly: System.Reflection.AssemblyProductAttribute("TPMOD4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TPMOD4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
