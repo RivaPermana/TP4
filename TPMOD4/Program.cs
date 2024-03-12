355dc406bab269db83a5d26534d54431cd38ce55
shad kinx <rivapermana763@gmail,com>
2024-03-13 06:10:39 +0700

    first

---
+++ TPMOD4/Program.cs
@@ -0,0 +1,23 @@
+﻿public class KodePos
+{
+    public enum EnumKelurahan
+    {
+        Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja
+    }
+
+    public static int GetKodePos(EnumKelurahan kelurahan)
+    {
+        int[] KodePosKelurahan = { 40266, 40287, 40267, 40287, 40286, 40286, 40272, 40274, 40273 };
+        return KodePosKelurahan[(int) kelurahan];
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Kelurahan & Kode Pos");
+        Console.WriteLine("---------------------");
+        foreach (EnumKelurahan kelurahan in Enum.GetValues(typeof(EnumKelurahan)))
+        {
+            Console.WriteLine($"{kelurahan} - {GetKodePos(kelurahan)}");
+        }
+    }
+}
